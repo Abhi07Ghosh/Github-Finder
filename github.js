ae2c9d4970376afea39d7f2e3d1d6f785e1d35bb
Abhi07Ghosh <abhi226ghosh@gmail.com>
2023-01-30 03:11:34 +0530

    first commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+      this.client_id = '84c5b999877bc934e0ec';
+      this.client_secret = '8eed973ce8f0b3c83fb6bf490e713b5e5bafcb72';
+      this.repos_count=10;
+      this.repos_sort='created: asc'
+    }
+  
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+    
+        return {
+          profile,
+          repos
+        }
+      }
+    }
